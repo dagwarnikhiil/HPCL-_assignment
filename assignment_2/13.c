fbe903e9e90f11d3283edd2117225905d689a219
Nikhil Dagwar <145436941+dagwarnikhiil@users.noreply.github.com>
2024-08-08 10:08:53 +0530

    Update 13.c

--- assignment_2/13.c
+++ assignment_2/13.c
@@ -1,12 +1,8 @@
-
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-   
-    printf("Command-line arguments:\n");
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-    }
+    
+    printf("Number of command-line arguments: %d\n", argc);
 
     return 0;
 }
